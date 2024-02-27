1b4eacd681e4fbf39531ffb4e56b370604cfc895
Matheus Pedrosa <106934941+Omatheuspedrosa@users.noreply.github.com>
2024-02-27 13:29:00 +0000

    ajustes no script

--- script.js
+++ script.js
@@ -3,25 +3,16 @@ document.getElementById("tema").addEventListener("click", function() {
     if (corAtual === "black") {
         document.body.style.backgroundColor = "white";
 
-        var paragrafos = document.querySelectorAll("p");
-        for (var k = 0; k < paragrafos.length; k++) {
-            paragrafos[k].style.color = ""; 
+        var textos = document.querySelectorAll("p, h1, h2, h3, h4, h5, h6, li, a");
+        for (var k = 0; k < textos.length; k++) {
+            textos[k].style.color = ""; 
         }
 
-        var headings = document.querySelectorAll("h1, h2, h3, h4, h5, h6, li, a");
-        for (var l = 0; l < headings.length; l++) {
-            headings[l].style.color = ""; 
-        }
     } else {
         document.body.style.backgroundColor = "black";
-        var paragrafos = document.querySelectorAll("p");
-        for (var i = 0; i < paragrafos.length; i++) {
-            paragrafos[i].style.color = "white";
-        }
-        var headings = document.querySelectorAll("h1, h2, h3, h4, h5, h6, li, a");
-        for (var j = 0; j < headings.length; j++) {
-            headings[j].style.color = "white";
+        var textos = document.querySelectorAll("p, h1, h2, h3, h4, h5, h6, li, a");
+        for (var i = 0; i < textos.length; i++) {
+            textos[i].style.color = "white";
         }
     }
 });
-
