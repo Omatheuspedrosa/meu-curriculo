8b5bdee9efcf1bf344dfbab6ed6cf876f00e24b1
Matheus Pedrosa <106934941+Omatheuspedrosa@users.noreply.github.com>
2024-02-27 12:17:07 +0000

    adicionado botão de tema

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-function tema() {
-    document.body.setAttribute("style", "background-color: red; font-size: 20px;");
-}
+document.getElementById("tema").addEventListener("click", function() {
+    var corAtual = document.body.style.backgroundColor;
+    if (corAtual === "gray") {
+        document.body.style.backgroundColor = "white";
+    } else {
+        document.body.style.backgroundColor = "gray";
+    }
+});
+
